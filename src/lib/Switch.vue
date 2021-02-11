14c980d2d96d4ff32a363f31fcbd92036970db34
e <shiokoy1209@gmail.com>
2021-02-11 16:36:25 +0800

    [switch]实现简单的点击切换和动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,12 +1,20 @@
 <template>
-  <button class="x-switch">
+  <button class="x-switch" :class="{ checked }" @click="toggle">
     <span></span>
   </button>
 </template>
 
 <script lang="ts">
-export default {
+import { ref } from 'vue'
 
+export default {
+  setup () {
+    const checked = ref(false)
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toggle}
+  }
 }
 </script>
 
@@ -17,8 +25,10 @@ export default {
   height: $switch-height;
   width: $switch-width;
   border: none;
-  background: $main-theme-color;
+  background: $gray-4;
   border-radius: $switch-height / 2;
+  cursor: pointer;
+  transition: background $switch-duration;
   >span {
     position: absolute;
     top: 2px;
@@ -27,9 +37,13 @@ export default {
     height: $switch-height - 4px;
     border-radius: $switch-height / 2;
     background: $white;
+    transition: left $switch-duration;
   }
-  &:hover >span {
-    left: calc(100%  - #{$switch-height} + 2px);
+  &.checked{
+    background: $main-theme-color;
+    >span {
+      left: calc(100%  - #{$switch-height} + 2px);
+    }
   }
 }
 </style>

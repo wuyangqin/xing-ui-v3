e22981792d2fc0bb365ad7c85f788c280ba8281b
e <shiokoy1209@gmail.com>
2021-02-15 21:44:46 +0800

    [switch]使用v-model实现双向绑定

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <x-switch></x-switch>
+    <x-switch v-model:value="bool"></x-switch>
   </div>
 </template>
 
 <script lang="ts">
 import XSwitch from '../lib/Switch.vue'
+import { ref } from 'vue';
 
 export default {
   name: 'switch-demo',
   components: {
     XSwitch
+  },
+  setup () {
+    const bool = ref(false)
+    return { bool }
   }
 }
 </script>

5482ae699cefa3e5e5d28e9b03cf1266c042a499
e <shiokoy1209@gmail.com>
2021-02-18 17:02:51 +0800

    [icon]添加icon组件

---
+++ src/lib/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="x-icon" :style="{ color }">
+    <use :xlink:href="`#x-${name}`"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: 'currentColor'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.xx-icon {
+  width: 1em;
+  height: 1em;
+  fill:currentColor;
+  color: currentColor;
+}
+</style>

47cc3feb0eee952bfbbed382f19a0ab66360e434
e <shiokoy1209@gmail.com>
2021-02-21 14:25:12 +0800

    [button]初步添加按钮组件,绑定属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    button文档
+    <x-button size="small"
+              @click="clickButton"
+              @mouseenter="clickButton"
+              @focus="clickButton">
+      点我
+    </x-button>
   </div>
 </template>
 
 <script lang="ts">
+import XButton from '../lib/Button.vue'
 
 export default {
   name: 'button-demo',
   components: {
+    XButton
+  },
+  methods: {
+    clickButton () {
+      console.log(1)
+    }
   }
 }
 </script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="x-button-dv" :size="size">
+    <button class="x-button" v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'x-button',
+  inheritAttrs: false,
+  setup (props, context) {
+    const { size, ...rest } = context.attrs // 将事件绑定给button，其余属性绑定给外层div
+    return { size, rest }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/css/xing-ui-v3.scss";
+.x-button-dv {
+  .x-button {
+    padding: .5em 1em;
+    background: $main-theme-color;
+    border: none;
+    border-radius: $border-radius;
+    color: $white;
+    cursor: pointer;
+  }
+}
+</style>

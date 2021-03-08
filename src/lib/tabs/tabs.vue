5284be8614e8a3707e4ffed547786997161658dd
e <shiokoy1209@gmail.com>
2021-03-08 17:43:41 +0800

    [tabs]判断子组件的类型

--- src/lib/tabs/tabs.vue
+++ src/lib/tabs/tabs.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="xx-tabs">
-    <slot></slot>
+<!--    <slot></slot>-->
+    <component :is="defaults[0]"/>
   </div>
 </template>
 
 <script lang="ts">
 
 export default {
-  name: 'xx-tabs'
+  name: 'xx-tabs',
+  setup (props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach(com => {
+      if (com.type.name !== 'xx-tab') {
+        throw new Error('Tabs 子标签必须是 Tab 组件')
+      }
+    })
+    return { defaults }
+  }
 }
 </script>
 

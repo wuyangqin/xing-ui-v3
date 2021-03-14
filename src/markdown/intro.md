36440205ac797e29d8a6bf5812de4c05e431a3df
e <shiokoy1209@gmail.com>
2021-03-14 21:41:22 +0800

    [docs]支持markdown文件

---
+++ src/markdown/intro.md
@@ -0,0 +1,11 @@
+# 介绍
+
+King UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。
+
+这款组件库是我在学习Vue3的过程中所编写，尽量不采用第三方库，包括官网也为自己编写。
+
+组件库仅供学习交流，请勿在生产环境中使用。
+
+欢迎讨论交流，如果你觉得还不错，请 [Star](https://github.com/wuyangqin/xing-ui-v3) o(*￣▽￣*)ブ。
+
+下一节：[安装](#/doc/install)
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,20 +1,15 @@
 <template>
-  <article class="markdown-body">
-    <h1>介绍</h1>
-    <p>Xing UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-    <p>这款组件库是我在学习Vue3的过程中所编写，尽量不采用第三方库，包括官网也为自己编写。</p>
-    <p>组件库仅供学习交流，请勿在生产环境中使用。 欢迎讨论交流，如果你觉得还不错，请 [Star](https://github.com/wuyangqin/xing-ui-v3) o(*￣▽￣*)ブ。</p>
-    下一节：<a href="#/doc/install">安装</a>
+  <article class="markdown-body" v-html="md">
   </article>
 </template>
 
 <script>
-
+import md from '../markdown/intro.md';
 export default {
-  name: 'intro'
+  data() {
+    return {
+      md
+    }
+  }
 }
 </script>
-
-<style scoped lang="scss">
-
-</style>

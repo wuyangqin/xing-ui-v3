4bd1898ce6653ac17235edfe58eeead9a3b4780e
e <shiokoy1209@gmail.com>
2021-03-15 10:26:49 +0800

    [docs]优化markdown

---
+++ src/components/Markdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="markdown-body" v-html="content">
+  </article>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'xx-markdown',
+  props: {
+    path:{
+      type: String,
+      default: ''
+    }
+  },
+  setup(props) {
+    const content = ref<string>('')
+    import(props.path).then((result) => {
+      content.value = result.default
+    })
+    return {
+      content
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@ import SwitchDemo from '../components/SwitchDemo.vue'
 import ButtonDemo from '../components/ButtonDemo.vue'
 import TabsDemo from '../components/TabsDemo.vue'
 import DialogDemo from '../components/DialogDemo.vue'
-import Intro from '../views/Intro.vue';
-import GetStarted from '../views/GetStarted.vue'
-import Install from '../views/Install.vue'
+import Markdown from '../components/Markdown.vue'
+import { h } from 'vue'
 import { createWebHashHistory, createRouter } from 'vue-router'
 
 const history = createWebHashHistory()
+const md = fileName => h(Markdown, { path:`../markdown/${fileName}.md`,key:fileName })
 export const router = createRouter({
     history,
     routes:[
@@ -20,9 +20,9 @@ export const router = createRouter({
             component:Doc,
             children: [
                 { path: '', component:DocDemo },
-                { path: 'intro', component:Intro  },
-                { path: "get-started", component: GetStarted },
-                { path: "install", component: Install },
+                { path: 'intro', component: md('intro')  },
+                { path: "get-started", component: md('get-started') },
+                { path: "install", component: md('install') },
                 { path: 'switch', component:SwitchDemo },
                 { path: 'button', component:ButtonDemo },
                 { path: 'dialog', component:DialogDemo },

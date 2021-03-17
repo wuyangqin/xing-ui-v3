b06dc9da98a8ac6172e1afa05554595feac41bb1
e <shiokoy1209@gmail.com>
2021-03-17 15:44:01 +0800

    [发布]创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as XSwitch } from './Switch.vue'
+export { default as XButton } from './Button.vue'
+export { default as XTabs } from './tabs/Tabs.vue'
+export { default as XTab } from './tabs/Tab.vue'
+export { default as XDialog } from './Dialog.vue'
+export { openDialog as openDialog } from './plugin/openDialog'
